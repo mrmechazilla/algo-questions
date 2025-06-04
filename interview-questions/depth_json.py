fcaa6c8ff33ea48ae303c88b0197137c0a21ddd2
youssef <mhamed.youssef.benyoussef@gmail.com>
2025-06-05 00:00:58 +0100

    feat: solve depth json problem with recursion

--- interview-questions/depth_json.py
+++ interview-questions/depth_json.py
@@ -1,2 +1,28 @@
-def depth_json():
-    ...
+def depth_json(obj, depth=0):
+    if not isinstance(obj, (dict, list)):
+        return depth
+
+    if (isinstance(obj, list) and len(obj) == 0) or (isinstance(obj, dict) and len(obj.keys()) == 0):
+        return depth + 1
+    else:
+        max_depth = depth
+        values = obj.values() if isinstance(obj, dict) else obj
+        for v in values:
+            local_depth = depth_json(v, depth + 1)
+            if local_depth > max_depth:
+                max_depth = local_depth
+        return max_depth
+    
+# Test Examples
+
+print(depth_json([]) == 1)
+print(depth_json([1, 2, 3, 4, 5]) == 1)
+print(depth_json([{ 'a': [] }, ["abc"]]) == 3)
+print(depth_json([{ 'a': { 'b': 1, 'c': 2 } }, ["abc"]]) == 3)
+print(depth_json([{ 'a': { 'b': 1, 'c': 2 } }, ["abc", {}]]) == 3)
+print(depth_json([{ 'a': { 'b': [10, 20], 'c': 2 } }, ["abc"]]) == 4)
+print(depth_json([{ 'a': { 'b': [10, 20, {}], 'c': 2 } }, ["abc"]]) == 5)
+print(depth_json([{ 'a': { 'b': [10, 20, { 'd': [] }], 'c': 2 } }, ["abc"]]) == 6)
+print(depth_json([{ 'a': { 'b': [10, 20, { 'd': [{}] }], 'c': 2 } }, ["abc"]]) == 7)
+print(depth_json([{ 'a': { 'b': [10, 20, { 'd': [{ 'e': [] }] }], 'c': 2 } }, ["abc"]]) == 8)
+print(depth_json([{ 'a': { 'b': [10, 20, { 'd': [{ 'e': [{}] }] }], 'c': 2 } }, ["abc"]]) == 9)
